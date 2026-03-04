4c184c92a03b1d37f52e1c8f1fbb85b982e699e6
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-03-04 19:36:39 +0600

    Featuted and some page section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,58 @@ import Car from './pages/Car';
 import MyBookings from './pages/MyBookings.JSX';
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false); // login modal control
+
   const isOwnerPath = useLocation().pathname.startsWith('/owner') // http://localhost:5173/owner path a gele navbar dekha jabe na
+
+  /*
+useLocation() 
+--> react-router-dom hook.
+--> It gives current url information
+--> (url change -> component re-render -> new location)
+
+
+.pathname 
+--> string
+--> url path
+
+
+.startsWith('/owner')
+--> JavaScript method
+--> Syntax: string.startsWith(searchValue)
+--> it check string ki nirdisto text diye start hoyeche kina.
+
+
+"/owner".startsWith("/owner") ----> Result → `true`
+
+"/owner/add-car".startsWith("/owner" ----> Result → `true`
+
+
+"/car".startsWith("/owner") ----> Result → `false`
+
+
+const isOwnerPath = useLocation().pathname.startsWith('/owner')
+--> current url
+--> pathname -> ber koro
+--> /owner -> diye suru hoyeche kina 
+--> hole -> true
+--> nahole -> false
+
+
+
+---> if user /owner related page -> then not show Navbar
+---> but if another page -> then show Navbar
+  
+  */ 
+  
   return (
     <>
      {!isOwnerPath && <Navbar ></Navbar> }
+     {/* if open owner dashboard then hide navbar */}
 
      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/car-details/:id' element={<CarDetails/>}/>
+      <Route path='/car-details/:id' element={<CarDetails/>}/> {/* dynamic ID route */}
       <Route path='/car' element={<Car/>}/>
       <Route path='/my-bookings' element={<MyBookings/>}/>
      </Routes>
